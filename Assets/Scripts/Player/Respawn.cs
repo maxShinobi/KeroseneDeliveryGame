4e92a712ac60a000b47a621b9374e44e261c08a3
Max Shinobi <mxshnb@gmail.com>
2022-09-10 19:12:11 +0900

    Respawn: complete fix

--- Assets/Scripts/Player/Respawn.cs
+++ Assets/Scripts/Player/Respawn.cs
@@ -5,7 +5,13 @@ public class Respawn : MonoBehaviour
     [SerializeField] int lostKeroseneValue;
 
     Vector3 originalPosition;
+    Quaternion originalRotation;
 
+    float originalPositionX;
+    float originalPositionY;
+    float originalPositionZ;
+
+    float originalRotationW;
     float originalRotationX;
     float originalRotationY;
     float originalRotationZ;
@@ -18,21 +24,29 @@ public class Respawn : MonoBehaviour
         activePlayerCar = StartGameCarSelection.instance.cars[StartGameCarSelection.instance.currentCarIndex];
         //currentPlayerKeroseneValue = PlayerPrefs.GetInt("AmountOfKerosene", PlayerKerosene.instance.currentValue);
 
+        originalPositionX = activePlayerCar.transform.position.x;
+        originalPositionY = activePlayerCar.transform.position.y;
+        originalPositionZ = activePlayerCar.transform.position.z;
+
+        originalRotationW = activePlayerCar.transform.rotation.w;
         originalRotationX = activePlayerCar.transform.rotation.x;
+        originalRotationY = activePlayerCar.transform.rotation.y;
         originalRotationZ = activePlayerCar.transform.rotation.z;
     }
 
     private void Update()
     {
-        originalPosition = new Vector3(activePlayerCar.transform.position.x, activePlayerCar.transform.position.y, activePlayerCar.transform.position.z);
+        originalPosition = new Vector3(originalPositionX, originalPositionY, originalPositionZ);
+        originalRotation = new Quaternion(originalRotationW, originalRotationX, originalRotationY, originalRotationZ);
     }
 
     public void RespawnCar()
     {
         originalPosition = new Vector3(activePlayerCar.transform.position.x, activePlayerCar.transform.position.y, activePlayerCar.transform.position.z);
-        activePlayerCar.transform.position = new Vector3(originalPosition.x, 2, originalPosition.z);
+        activePlayerCar.transform.position = new Vector3(originalPosition.x, 4, originalPosition.z);
 
-        activePlayerCar.transform.rotation = Quaternion.Euler(originalRotationX, originalRotationY, originalRotationZ);
+        originalRotation = Quaternion.Euler(activePlayerCar.transform.rotation.x, activePlayerCar.transform.rotation.y, activePlayerCar.transform.rotation.z);
+        activePlayerCar.transform.rotation = Quaternion.Euler(0f, activePlayerCar.transform.eulerAngles.y, 0f);
 
         PlayerLosesSomeKerosene();
     }
